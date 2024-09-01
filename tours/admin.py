ed3d37e4452260bd6c7c8a5bd909be29605f1e6e
Anton Ratkin <abstract.core@gmail.com>
2024-09-01 17:08:55 +0300

    Adds Order model and admin for it. Fixes type of ship field for Tour model.

--- tours/admin.py
+++ tours/admin.py
@@ -1,78 +1,113 @@
 from django.contrib import admin
 
-from tours.models import Tour, Photo, TourDate, FreeOfChargeService, AdditionalOption, \
-    Route, Ship, CabinType, Lunch
+from tours.models import (
+    AdditionalOption,
+    CabinType,
+    FreeOfChargeService,
+    Lunch,
+    Order,
+    Photo,
+    Route,
+    Ship,
+    Tour,
+    TourDate,
+)
 
 
 @admin.register(Tour)
 class TourAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_date', 'pick_up_time', 'duration', 'cost',
-                    'ship', 'author')
-    list_editable = ('start_date', 'pick_up_time', 'duration', 'cost', 'ship')
-    search_fields = ('name',)
+    list_display = (
+        "name",
+        "start_date",
+        "pick_up_time",
+        "duration",
+        "cost",
+        "ship",
+        "author",
+    )
+    list_editable = ("start_date", "pick_up_time", "duration", "cost", "ship")
+    search_fields = ("name",)
     filter_horizontal = (
-        'tour_photo_gallery', 'free_services', 'additional_options', 'lunch',
-        'route')
-    exclude = ('author',)
+        "tour_photo_gallery",
+        "free_services",
+        "additional_options",
+        "lunch",
+        "route",
+    )
+    exclude = ("author",)
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         # Restrict tour providers to see only their own tours
-        if request.user.groups.filter(name='Tour Provider').exists():
+        if request.user.groups.filter(name="Tour Provider").exists():
             return qs.filter(author=request.user)
         return qs
 
     def has_change_permission(self, request, obj=None):
         # Allow tour providers to edit only their own tours
-        if obj is not None and request.user.groups.filter(
-                name='Tour Provider').exists():
+        if (
+            obj is not None
+            and request.user.groups.filter(name="Tour Provider").exists()
+        ):
             return obj.author == request.user
         return super().has_change_permission(request, obj)
 
     def has_view_permission(self, request, obj=None):
         # Allow tour providers to view only their own tours
-        if obj is not None and request.user.groups.filter(
-                name='Tour Provider').exists():
+        if (
+            obj is not None
+            and request.user.groups.filter(name="Tour Provider").exists()
+        ):
             return obj.author == request.user
         return super().has_view_permission(request, obj)
 
     def has_add_permission(self, request):
         # Allow tour providers to add tours
-        if request.user.groups.filter(name='Tour Provider').exists():
+        if request.user.groups.filter(name="Tour Provider").exists():
             return True
         return super().has_add_permission(request)
 
     def has_delete_permission(self, request, obj=None):
         # Tour providers should not have permission to delete tours
-        if request.user.groups.filter(name='Tour Provider').exists():
+        if request.user.groups.filter(name="Tour Provider").exists():
             return False
         return super().has_delete_permission(request, obj)
 
     def save_model(self, request, obj, form, change):
         if not change or obj.author is None:
-            # Automatically set the author to the logged-in user if this is a new object or if author is not set
+            # Automatically set the author to the
+            # logged-in user if this is a new object or if author is not set
             obj.author = request.user
         super().save_model(request, obj, form, change)
 
 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("tour", "date", "email_of_initiator", "phone_number", "messenger")
+    search_fields = ("email_of_initiator", "phone_number", "messenger")
+
+
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('description',)
+    list_display = ("description",)
 
 
 @admin.register(TourDate)
 class TourDateAdmin(admin.ModelAdmin):
-    list_display = ('date',)
+    list_display = ("date",)
 
 
 @admin.register(FreeOfChargeService)
 class FreeOfChargeServiceAdmin(admin.ModelAdmin):
-    list_display = ('service_name', 'service_description',)
+    list_display = (
+        "service_name",
+        "service_description",
+    )
 
 
 @admin.register(AdditionalOption)
 class AdditionalOptionAdmin(admin.ModelAdmin):
-    list_display = ('option_name', 'option_description')
+    list_display = ("option_name", "option_description")
 
 
 @admin.register(Route)
--- tours/models.py
+++ tours/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -22,7 +23,7 @@ class Lunch(models.Model):
 class CabinType(models.Model):
     type = models.CharField(max_length=50)
     description = models.TextField()
-    photo = models.ImageField(upload_to='cabins/', blank=True)
+    photo = models.ImageField(upload_to="cabins/", blank=True)
 
     def __str__(self):
         return self.description
@@ -32,7 +33,7 @@ class Ship(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50)
     description = models.TextField()
-    photo = models.ImageField(upload_to='images/', blank=True)
+    photo = models.ImageField(upload_to="images/", blank=True)
     amount_of_places = models.IntegerField()
     cabin_types = models.ForeignKey("CabinType", on_delete=models.CASCADE)
 
@@ -43,7 +44,7 @@ class Ship(models.Model):
 class Route(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
-    photo = models.ManyToManyField('Photo', blank=True)
+    photo = models.ManyToManyField("Photo", blank=True)
     duration = models.DurationField()
 
     def __str__(self):
@@ -52,7 +53,7 @@ class Route(models.Model):
 
 class FreeOfChargeService(models.Model):
     service_name = models.CharField(max_length=50, default="Default Service")
-    service_description = models.TextField(blank=True, null=True)
+    service_description = models.TextField(blank=True, default="Default Description")
 
     def __str__(self):
         return self.service_name
@@ -60,36 +61,54 @@ class FreeOfChargeService(models.Model):
 
 class AdditionalOption(models.Model):
     option_name = models.CharField(max_length=50, default="Default Option")
-    option_description = models.TextField(blank=True, null=True)
+    option_description = models.TextField(blank=True, default="Default Description")
 
     def __str__(self):
         return self.option_name
 
 
 class Photo(models.Model):
-    image = models.ImageField(upload_to='tours/gallery/')
-    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="tours/gallery/")
+    description = models.TextField(blank=True, default="Default Description")
+
+    def __str__(self):
+        return self.description
 
 
 class Tour(models.Model):
     name = models.CharField(max_length=50)
     tour_description = models.TextField()
-    tour_photo_gallery = models.ManyToManyField('Photo', blank=True)
+    tour_photo_gallery = models.ManyToManyField("Photo", blank=True)
     route = models.ManyToManyField("Route", blank=True)
-    ship = models.OneToOneField("Ship", on_delete=models.SET_NULL,
-                                   null=True, blank=True)
+    ship = models.ForeignKey("Ship", on_delete=models.SET_NULL, null=True, blank=True)
     lunch = models.ManyToManyField("Lunch", blank=True)
-    start_date = models.ForeignKey('TourDate', on_delete=models.SET_NULL,
-                                   null=True, blank=True)
+    start_date = models.ForeignKey(
+        "TourDate", on_delete=models.SET_NULL, null=True, blank=True
+    )
     duration = models.DurationField()
     pick_up_time = models.TimeField()
-    free_services = models.ManyToManyField('FreeOfChargeService', blank=True)
-    additional_options = models.ManyToManyField('AdditionalOption', blank=True)
-    cost = models.DecimalField(max_digits=10,
-                               decimal_places=2)
-    cost_options = models.TextField(blank=True,
-                                    null=True)
+    free_services = models.ManyToManyField("FreeOfChargeService", blank=True)
+    additional_options = models.ManyToManyField("AdditionalOption", blank=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_options = models.TextField(blank=True, default="Default Cost Options")
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # For roles segregation
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    tour = models.ForeignKey("Tour", on_delete=models.CASCADE)
+    passengers = models.TextField()
+    date = models.DateField(default=timezone.now)
+    email_of_initiator = models.EmailField()
+    phone_number = models.CharField(max_length=50)
+    messenger = models.CharField(max_length=50)
+    cost_per_person = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_of_tour = models.DecimalField(max_digits=10, decimal_places=2)
+    name_of_tour = models.TextField()
+    duration_of_tour = models.DurationField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name_of_tour
