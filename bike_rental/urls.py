d28ea9f3708a0c51b525607c9747c757d02efda6
mikiminimouse <skullbv@gmail.com>
2024-09-07 01:06:35 +0700

    working stage (fix images photo_bike)

--- bike_rental/urls.py
+++ bike_rental/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import BikeModelListView, bikemodel_detail, bike_offer, bike_order, order_confirmation
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('bikemodels/', BikeModelListView.as_view(), name='bikemodel_list'),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('bike/<int:id>/order/', bike_order, name='bike_order'),
     path('order/<int:order_id>/confirmation/', order_confirmation, name='order_confirmation'),
 ]
+
+if settings.DEBUG:
+    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
