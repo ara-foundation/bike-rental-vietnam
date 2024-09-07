c99ce144acb134511b88faa733e2d9b6afcb4abc
mikiminimouse <skullbv@gmail.com>
2024-09-07 15:18:22 +0700

    change desing front page

--- bike_rental/views.py
+++ bike_rental/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django_filters.views import FilterView
 from django.core.paginator import Paginator
 from .models import BikeModel, Bike, Order
 from .filters import BikeModelFilter
 from django.urls import reverse
 from .forms import ClientForm, OrderForm
+from django.views.generic import ListView
 
-class BikeModelListView(FilterView):
+class BikeModelListView(ListView):
     model = BikeModel
-    filterset_class = BikeModelFilter
     template_name = 'bikemodel_list.html'
-    paginate_by = 10
+    context_object_name = 'bikemodels'
+    paginate_by = 9  # Установите количество объектов на страницу
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = BikeModelFilter(self.request.GET, queryset=queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = self.filterset
+        return context
 
 def bikemodel_detail(request, id):
     bikemodel = get_object_or_404(BikeModel, id=id)
@@ -56,3 +67,27 @@ def order_confirmation(request, order_id):
     order = get_object_or_404(Order, id=order_id)
     context = {'order': order}
     return render(request, 'order_confirmation.html', context)
+
+def get_models(request):
+    brand_id = request.GET.get('brand')
+    models = BikeModel.objects.filter(brand_id=brand_id).values('id', 'model')
+    return JsonResponse(list(models), safe=False)
+
+def get_transmissions(request):
+    model_id = request.GET.get('model')
+    transmissions = BikeModel.objects.filter(id=model_id).values_list('transmission', flat=True).distinct()
+    return JsonResponse(list(transmissions), safe=False)
+
+def get_models_and_transmissions(request):
+    brand_id = request.GET.get('brand_id')
+    models_and_transmissions = BikeModel.objects.filter(brand_id=brand_id).values('id', 'model', 'transmission').distinct()
+    return JsonResponse(list(models_and_transmissions), safe=False)
+
+def get_brand_and_transmission(request):
+    model_id = request.GET.get('model_id')
+    bike_model = BikeModel.objects.get(id=model_id)
+    data = {
+        'brand_id': bike_model.brand_id,
+        'transmission': bike_model.transmission
+    }
+    return JsonResponse(data)
