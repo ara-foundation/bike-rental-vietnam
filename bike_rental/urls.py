49efb4d3b42605c7a6aca34bc19de6127f1437ac
mikiminimouse <skullbv@gmail.com>
2024-09-10 15:55:43 +0700

    Sketches of templates for tours are made.

--- bike_rental/urls.py
+++ bike_rental/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BikeModelListView, bikemodel_detail, bike_offer, bike_order, order_confirmation, bike_tours, car_tours, bus_tours
+from .views import BikeModelListView, bikemodel_detail, bike_offer, bike_order, order_confirmation, bike_tours, car_tours, boat_tours, bike_tour_order,bike_tour
 
 urlpatterns = [
     # Существующие маршруты
@@ -11,7 +11,9 @@ urlpatterns = [
     path('order/<int:order_id>/confirmation/', order_confirmation, name='order_confirmation'),
     path('bike-tours/', bike_tours, name='bike_tours'),
     path('car-tours/', car_tours, name='car_tours'),
-    path('bus-tours/', bus_tours, name='bus_tours'),
+    path('boats-tours/', boat_tours, name='boat_tours'),
+    path('bike-tour-order/<int:tour_id>/', bike_tour_order, name='bike_tour_order'),
+    path('bike-tour/<int:tour_id>/', bike_tour, name='bike_tour'),
 ]
 # from django.conf import settings
 # from django.conf.urls.static import static
