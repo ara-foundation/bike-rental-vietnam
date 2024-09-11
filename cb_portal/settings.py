7f1b8056fa8b446a6c08a250fd09bd5fbdeb93fa
mikiminimouse <skullbv@gmail.com>
2024-09-12 03:15:53 +0700

    in settings.py added whitenoise in MIDDLEWARE list

--- cb_portal/settings.py
+++ cb_portal/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'cb_portal.urls'
